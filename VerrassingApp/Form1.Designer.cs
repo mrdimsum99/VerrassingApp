e52b2fb60609fca24586ef1056ba35a13249e849
mrdimsum99 <deonkidjo_18@hotmail.com>
2018-03-21 14:47:33 +0100

    App is gecreëerd

---
+++ VerrassingApp/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace VerrassingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnTeam4 = new System.Windows.Forms.Button();
+            this.btnTeam3 = new System.Windows.Forms.Button();
+            this.btnTeam2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnTeam1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(191, 190);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(428, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(191, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(428, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(428, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // btnTeam4
+            // 
+            this.btnTeam4.Location = new System.Drawing.Point(37, 188);
+            this.btnTeam4.Name = "btnTeam4";
+            this.btnTeam4.Size = new System.Drawing.Size(148, 23);
+            this.btnTeam4.TabIndex = 16;
+            this.btnTeam4.Text = "Uiteindelijk";
+            this.btnTeam4.UseVisualStyleBackColor = true;
+            this.btnTeam4.Click += new System.EventHandler(this.btnTeam4_Click);
+            // 
+            // btnTeam3
+            // 
+            this.btnTeam3.Location = new System.Drawing.Point(37, 159);
+            this.btnTeam3.Name = "btnTeam3";
+            this.btnTeam3.Size = new System.Drawing.Size(148, 23);
+            this.btnTeam3.TabIndex = 15;
+            this.btnTeam3.Text = "Wat er toen gebeurde";
+            this.btnTeam3.UseVisualStyleBackColor = true;
+            this.btnTeam3.Click += new System.EventHandler(this.btnTeam3_Click);
+            // 
+            // btnTeam2
+            // 
+            this.btnTeam2.Location = new System.Drawing.Point(37, 130);
+            this.btnTeam2.Name = "btnTeam2";
+            this.btnTeam2.Size = new System.Drawing.Size(148, 23);
+            this.btnTeam2.TabIndex = 14;
+            this.btnTeam2.Text = "Op een dag";
+            this.btnTeam2.UseVisualStyleBackColor = true;
+            this.btnTeam2.Click += new System.EventHandler(this.btnTeam2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Klik op de buttons om de zinnen te zien";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 26);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "VerhaalApp";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // btnTeam1
+            // 
+            this.btnTeam1.Location = new System.Drawing.Point(37, 101);
+            this.btnTeam1.Name = "btnTeam1";
+            this.btnTeam1.Size = new System.Drawing.Size(148, 23);
+            this.btnTeam1.TabIndex = 10;
+            this.btnTeam1.Text = "Er was eens";
+            this.btnTeam1.UseVisualStyleBackColor = true;
+            this.btnTeam1.Click += new System.EventHandler(this.btnTeam1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 261);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnTeam4);
+            this.Controls.Add(this.btnTeam3);
+            this.Controls.Add(this.btnTeam2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTeam1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnTeam4;
+        private System.Windows.Forms.Button btnTeam3;
+        private System.Windows.Forms.Button btnTeam2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnTeam1;
+    }
+}
+
---
+++ VerrassingApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerrassingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeam1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btnTeam2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void btnTeam3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+        private void btnTeam4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+        }
+    }
+}
